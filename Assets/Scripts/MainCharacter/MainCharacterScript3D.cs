fe9246532848e16cd489549ce429b344be718911
COUETTE Allan <55735176+CouetteAllan@users.noreply.github.com>
2023-01-31 22:23:46 +0100

    Start 3D project

---
+++ Assets/Scripts/MainCharacter/MainCharacterScript3D.cs
@@ -0,0 +1,291 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MainCharacterScript3D : Entity
+{
+    [SerializeField] private Vector2 acceleration;
+    [SerializeField] private Vector2 decceleration;
+    [SerializeField] private Vector2 velocityPower;
+
+    [SerializeField] private float invincibleTime = 2f;
+    [SerializeField] private float dashInvincibleTime = 1f;
+    [SerializeField] private float dashForce = 1f;
+    private bool isDashing = false;
+    private bool canDash = true;
+    [SerializeField] private float dashCooldown = 1f;
+    [SerializeField] private TrailRenderer trail;
+
+    [SerializeField] private Vector2 input;
+    private Vector2 lastInput;
+
+    public PlayerInputAction playerInputAction;
+    private PlayerInput playerInput;
+    [HideInInspector] public Vector2 mouseAim;
+
+    [HideInInspector] public bool isFlipped = false;
+    private float time = 0f;
+
+    private PlayerAttack3D playerAttackScript;
+    private Weapons weaponPickedUpData;
+
+    private bool isMoving => input != Vector2.zero;
+
+    public bool startWithWeapon = false;
+    [HideInInspector] public Weapons weapon;
+
+    private bool canMove = true;
+
+    private Coroutine invincibleCoroutine;
+    public delegate void PlayerChangeHealth(float value);
+    public PlayerChangeHealth OnPlayerChangeHealth;
+
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInputAction = new PlayerInputAction();
+        playerAttackScript = GetComponent<PlayerAttack3D>();
+
+        playerInputAction.Player.Enable();
+        playerInputAction.Player.Attack.performed += playerAttackScript.OnAttack;
+        playerInputAction.Player.MouseAim.performed += OnMouseChangePos;
+        playerInputAction.Player.Dash.performed += Dash;
+    }
+
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        //GameManager.Instance.InitPlayer(this);
+        if (startWithWeapon)
+        {
+            PickUpWeapon(weapon);
+            playerAttackScript.SetAttackSpeed(weaponPickedUpData.attackRate);
+        }
+        trail.emitting = false;
+        health = maxHealth;
+        //UIManager.Instance.SetMaxHealth(maxHealth,this);
+        //UIManager.Instance.SetUIHealth(maxHealth);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            input = playerInputAction.Player.Move.ReadValue<Vector2>();
+            if (Mathf.Abs(input.x) > 0.009f || Mathf.Abs(input.y) > 0.009f)
+            {
+                lastInput = input;
+
+            }
+        }
+        if (Mathf.Abs(input.x) > 0.009f || Mathf.Abs(input.y) > 0.009f)
+            playerAttackScript.LastInput = lastInput;
+        if(input.x > 0.01f && isFlipped)
+        {
+            Flip();
+        }
+        else if(input.x < -0.01f && !isFlipped)
+        {
+            Flip();
+        }
+
+        animator.SetBool("IsMoving", isMoving);
+
+
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            EnemyManager.Instance.SpawnEnemy();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        if (isDashing)
+            return;
+        var targetSpeed = movementSpeed * input;
+        var speedDiff = targetSpeed - rb2D.velocity;
+        var accelRate = new Vector2(Mathf.Abs(targetSpeed.x) > 0.01f ? acceleration.x : decceleration.x, Mathf.Abs(targetSpeed.y) > 0.01f ? acceleration.y : decceleration.y);
+        var movement = new Vector2(Mathf.Pow(Mathf.Abs(speedDiff.x) * accelRate.x, velocityPower.x) * Mathf.Sign(speedDiff.x), Mathf.Pow(Mathf.Abs(speedDiff.y) * accelRate.y, velocityPower.y) * Mathf.Sign(speedDiff.y));
+
+        this.rb2D.AddForce(movement,ForceMode2D.Impulse);
+    }
+
+    private void Dash(InputAction.CallbackContext context)
+    {
+        if (!canMove || !canDash)
+            return;
+
+        StartCoroutine(DashCoroutine());
+        
+    }
+
+    IEnumerator DashCoroutine()
+    {
+        if (invincibleCoroutine != null)
+            StopCoroutine(invincibleCoroutine);
+        invincibleCoroutine = StartCoroutine(DashInvincibilityHandle(dashInvincibleTime));
+        //un delay de 6frames (je devrai peut-être adapter en seconde)
+        yield return new WaitForSeconds(0.07f);
+
+
+        isDashing = true;
+        //Va rapidement dans une direction
+        this.rb2D.velocity = Vector2.zero;
+        this.rb2D.AddForce(lastInput.normalized * dashForce, ForceMode2D.Impulse);
+        trail.emitting = true;
+
+
+        //Ne peut pas input de direction pendant un certains temps
+        StartCoroutine(CantMoveTimer(0.4f));
+    }
+
+    IEnumerator DashCooldownTimer(float timer)
+    {
+        canDash = false;
+        yield return new WaitForSeconds(timer);
+        canDash = true;
+        yield break;
+    }
+
+    public void Flip()
+    {
+        isFlipped = !isFlipped;
+        Vector3 currentScale = animator.gameObject.transform.localScale;
+        currentScale.x *= -1;
+        animator.gameObject.transform.localScale = currentScale;
+        
+
+    }
+
+    public void PickUpWeapon(Weapons weapon)
+    {
+        weaponPickedUpData = weapon;
+        playerAttackScript.SetActualWeapon(weaponPickedUpData);
+        this.damage = weaponPickedUpData.damage;
+    }
+
+    private void OnMouseChangePos(InputAction.CallbackContext context)
+    {
+        mouseAim = context.action.ReadValue<Vector2>();
+        Vector2 realMousePos = Camera.main.ScreenToWorldPoint(new Vector3(mouseAim.x,mouseAim.y,Camera.main.nearClipPlane));
+        Vector2 dir = realMousePos - rb2D.position;
+        playerAttackScript.MouseAimDir = dir;
+    }
+
+    public Animator GetAnimator()
+    {
+        return animator;
+    }
+
+    public Rigidbody2D GetRigidbody2D()
+    {
+        return rb2D;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        IPickable pickable = collision.GetComponent<IPickable>();
+        if(pickable != null)
+        {
+            pickable.OnPick(this);
+        }
+
+        /*if(collision.TryGetComponent<IExit>(out IExit exit))
+        {
+            exit.OnExit(this);
+        }*/
+
+    }
+
+    public override void OnHit(float _value, IHitSource source)
+    {
+
+        base.OnHit(_value, source);
+        StartCoroutine(InvincibilityHandle(invincibleTime));
+    }
+
+    public override void ChangeHealth(float _value, IHitSource source = null)
+    {
+        base.ChangeHealth(_value, source);
+        OnPlayerChangeHealth(health + _value);
+    }
+    public override void Die()
+    {
+        GameManager.Instance.PlayerDeath();
+        base.Die();
+    }
+
+    private void OnDestroy()
+    {
+        playerInputAction.Player.Disable();
+        playerInputAction.Player.Attack.performed -= playerAttackScript.OnAttack;
+        playerInputAction.Player.MouseAim.performed -= OnMouseChangePos;
+    }
+
+    IEnumerator InvincibilityHandle(float time)
+    {
+        isInvincible = true;
+        Physics2D.IgnoreLayerCollision(6, 8, true);
+        Physics2D.IgnoreLayerCollision(6, 9, true);
+        SpriteRenderer sprite = graphObject.GetComponent<SpriteRenderer>();
+        sprite.color = new Color(1f, 1f, 1f, 0.4f);
+
+        yield return new WaitForSeconds(time);
+        Physics2D.IgnoreLayerCollision(6, 8, false);
+        Physics2D.IgnoreLayerCollision(6, 9, false);
+        sprite.color = new Color(1f, 1f, 1f, 1f);
+        isInvincible = false;
+        yield break;
+    }
+    
+    IEnumerator DashInvincibilityHandle(float time)
+    {
+        isInvincible = true;
+
+        Physics2D.IgnoreLayerCollision(6, 8, true);
+        Physics2D.IgnoreLayerCollision(6, 9, true);
+        SpriteRenderer sprite = graphObject.GetComponent<SpriteRenderer>();
+
+        sprite.color = new Color(1f, 1f, 1f, 0.4f);
+        yield return new WaitForSeconds(time);
+        Physics2D.IgnoreLayerCollision(6, 8, false);
+        Physics2D.IgnoreLayerCollision(6, 9, false);
+        sprite.color = new Color(1f, 1f, 1f, 1f);
+        isInvincible = false;
+        isDashing = false;
+
+        yield break;
+    }
+
+    IEnumerator CantMoveTimer(float timer)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(timer);
+        canMove = true;
+        trail.emitting = false;
+        isDashing = false;
+        StartCoroutine(DashCooldownTimer(dashCooldown));
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/MainCharacter/PlayerAttack3D.cs
@@ -0,0 +1,262 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack3D : MonoBehaviour, IHitSource
+{
+    private bool isUsingGamePad;
+    [SerializeField] GameObject hand;
+
+    private Weapons actualWeapon;
+    private float attackSpeed = 1f;
+    private PlayerInput playerInput;
+    private MainCharacterScript3D player;
+    [SerializeField] LayerMask layerAttack;
+
+    public Vector2 range;
+    public Vector2 offset;
+
+    private Rigidbody2D rb2D;
+    public GameObject hitbox;
+
+
+    public bool isAttacking = false;
+    public Vector2 MouseAimDir { get; set; }
+    private Vector2 lastInput;
+    public Vector2 LastInput { get; set; }
+
+    [SerializeField] private float pushForceForward = 6f;
+
+    public delegate void Attack();
+    public Attack attackHandle;
+    public Attack aimingHandle;
+
+
+    public Rigidbody2D SourceRigidbody2D => this.rb2D;
+    public float Damage => actualWeapon.damage;
+
+    public bool IsDead => false;
+
+    private Coroutine attackCoroutine;
+    private delegate void AttackType();
+    private AttackType attackType;
+
+    private List<IHittable> enemiesThatBeenHit = new List<IHittable>();
+    
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+        player = GetComponent<MainCharacterScript3D>();
+    }
+
+    private void Start()
+    {
+        if (isUsingGamePad)
+        {
+            attackHandle = GamePadAttackHandle;
+            aimingHandle = GamePadAimingHandle;
+
+        }
+        else
+        {
+            attackHandle = MouseAttackHandle;
+            aimingHandle = MouseAimingHandle;
+
+        }
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInput.onControlsChanged += PlayerInput_onControlsChanged;
+
+        player.GetAnimator().SetFloat("AttackSpeedModifier", attackSpeed);
+    }
+
+    private void PlayerInput_onControlsChanged(PlayerInput obj)
+    {
+        if (obj.currentControlScheme == "Gamepad")
+        {
+            attackHandle = GamePadAttackHandle;
+            aimingHandle = GamePadAimingHandle;
+        }
+        else
+        {
+            attackHandle = MouseAttackHandle;
+            aimingHandle = MouseAimingHandle;
+        }
+
+
+    }
+
+    public void SetAttackSpeed(float _attackspeed)
+    {
+        attackSpeed = _attackspeed;
+    }
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (isAttacking)
+            return;
+        //Animation d'attaque
+        player.GetAnimator().SetTrigger("Attack");
+        rb2D.velocity = Vector2.zero;
+        isAttacking = true;
+
+        //la visée est choisit en fonction de la manette ou bien de la souris
+        aimingHandle();
+    }
+
+    public void ActivateHitBox()
+    {
+        if (actualWeapon == null)
+            return;
+        attackHandle();
+    }
+
+
+    private void MouseAimingHandle()
+    {
+        if (MouseAimDir.normalized.x > 0.01f && player.isFlipped == true)
+        {
+            player.Flip();
+        }
+        else if (MouseAimDir.normalized.x < -0.01f && player.isFlipped == false)
+        {
+            player.Flip();
+        }
+    }
+
+    private void MouseAttackHandle()
+    {
+        rb2D.AddForce(MouseAimDir.normalized * pushForceForward, ForceMode2D.Impulse);
+        hitbox.SetActive(true);
+        offset = rb2D.position + MouseAimDir.normalized;
+
+        attackType();
+    }
+
+    private void GamePadAimingHandle()
+    {
+        Collider2D[] enemiesInFront = Physics2D.OverlapCircleAll(rb2D.position + LastInput.normalized * 1.7f, 2.6f, layerAttack);
+        Vector2 nearestDir = LastInput;
+        Vector2 nearestPos;
+        float minDistSquared = Mathf.Infinity;
+        Vector2 currentPos = this.rb2D.position;
+
+        foreach (var enemy in enemiesInFront)
+        {
+            Rigidbody2D rbEnemy = enemy.GetComponent<Rigidbody2D>();
+
+            Vector3 directionToTarget = rbEnemy.position - currentPos;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < minDistSquared)
+            {
+                minDistSquared = dSqrToTarget;
+                nearestPos = rbEnemy.position;
+                nearestDir = nearestPos - currentPos;
+            }
+
+        }
+        lastInput = nearestDir.normalized;
+    }
+
+    private void GamePadAttackHandle()
+    {
+        rb2D.AddForce(lastInput.normalized * pushForceForward, ForceMode2D.Impulse);
+        hitbox.SetActive(true);
+
+        offset = rb2D.position + lastInput.normalized ;
+
+        attackType();
+        
+    }
+
+    private void MeleeAttack()
+    {
+        if (enemiesThatBeenHit != null)
+        {
+            foreach (IHittable enemy in enemiesThatBeenHit)
+            {
+                enemy.GotHit = false;
+            }
+        }
+        enemiesThatBeenHit.Clear();
+        if (attackCoroutine != null)
+            StopCoroutine(attackCoroutine);
+        attackCoroutine = StartCoroutine(ActiveFrameAttack(isUsingGamePad));
+        hitbox.transform.position = new Vector3(offset.x, offset.y, 0);
+        hitbox.transform.localScale = new Vector3((actualWeapon.range / 20) * 1.4f, (actualWeapon.range / 20) * 1.4f, 0);
+    }
+
+    IEnumerator ActiveFrameAttack(bool usingGamePad)
+    {
+        Vector2 mouseAim = MouseAimDir;
+        
+        while (isAttacking)
+        {
+            if(usingGamePad)
+                offset = rb2D.position + lastInput.normalized ;
+            else
+                offset = rb2D.position + mouseAim.normalized;
+
+            hitbox.transform.position = new Vector3(offset.x, offset.y, 0);
+            Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(offset, actualWeapon.range / 20, layerAttack);
+            if(enemiesHit != null)
+            {
+                foreach (var enemy in enemiesHit)
+                {
+                    IHittable hitObject = enemy.GetComponent<IHittable>();
+                    hitObject.OnHit(-actualWeapon.damage, this);
+                    if (!hitObject.GotHit)
+                    {
+                        enemiesThatBeenHit.Add(hitObject);
+                        hitObject.GotHit = true;
+                    }
+
+                }
+            }
+            
+            yield return new WaitForSeconds(0.015f);
+        }
+
+        yield break;
+
+    }
+
+    private void RangedAttack()
+    {
+        hitbox.transform.position = new Vector3(offset.x, offset.y, 0);
+        GameObject projectile = Instantiate(actualWeapon.projectile, this.rb2D.position, Quaternion.identity);
+        projectile.layer = 6;
+        projectile.GetComponent<ProjectileScript>().GotHit = true;
+        projectile.GetComponent<ProjectileScript>().SetProjectile(offset - rb2D.position, 12f, actualWeapon.damage,player);
+    }
+
+    public void DeActivateHitBox()
+    {
+        //desactive la hitbox en fin d'animation
+        hitbox.SetActive(false);
+        isAttacking = false;
+    }
+
+    public void SetActualWeapon(Weapons weapon)
+    {
+        this.actualWeapon = weapon;
+        this.attackSpeed = weapon.attackRate;
+        switch (actualWeapon.weaponType)
+        {
+            case Weapons.WeaponType.Melee:
+                attackType = MeleeAttack;
+                break;
+            case Weapons.WeaponType.Ranged:
+                attackType = RangedAttack;
+                break;
+        }
+        hand.GetComponent<SpriteRenderer>().sprite = actualWeapon.sprite;
+    }
+
+
+    /*private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(offset, actualWeapon.range / 20);
+    }*/
+}
