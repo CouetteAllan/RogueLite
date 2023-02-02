a216084cc49c31b949800d08bd51ec89649bef7d
COUETTE Allan <55735176+CouetteAllan@users.noreply.github.com>
2023-02-03 00:36:13 +0100

    tyj

--- Assets/ChangeCameraScript.cs
+++ Assets/ChangeCameraScript.cs
@@ -5,23 +5,6 @@ using UnityEngine;
 public class ChangeCameraScript : MonoBehaviour
 {
     [SerializeField] private GameObject virtualCam;
-
-    /*private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.TryGetComponent<MainCharacterScript>(out MainCharacterScript player) && !other.isTrigger)
-        {
-            virtualCam.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.TryGetComponent<MainCharacterScript>(out MainCharacterScript player) && !other.isTrigger)
-        {
-            virtualCam.SetActive(false);
-        }
-    }*/
-
     public void ChangeCamera(GameObject _nextVirtualCamera)
     {
         virtualCam.SetActive(false);
